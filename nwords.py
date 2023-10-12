48af842ae249c8ae59d8a7d276d49c59772246fc
Malavika <146753226+malvikaaaaa@users.noreply.github.com>
2023-10-13 00:03:26 +0530

    Update nwords.py

--- nwords.py
+++ nwords.py
@@ -1,6 +1,7 @@
 words=input("Enter the list of words:")
 wordslist=words.split(',')
 num=int(input("\nEnter a number:"))
+print("Number longer than ",num,"is:")
 for i in wordslist:
     if(len(i)>num):
         print(i)
