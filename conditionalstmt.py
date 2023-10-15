139cad3b62054600d2120564733e9bf01d27163c
Malavika <146753226+malvikaaaaa@users.noreply.github.com>
2023-10-15 18:07:29 +0530

    Update conditionalstmt.py

--- conditionalstmt.py
+++ conditionalstmt.py
@@ -3,6 +3,9 @@ result='Even' if not n%2 else 'Odd'
 print("The number is",result)
 
 l=input("Enter a list:")
+l=l.split(',')
 value=input("Enter a value:")
+l=list(map(str,l))
 result1='Available' if value in l else 'Not available'
 print(value,"is",result1,"in list")
+
