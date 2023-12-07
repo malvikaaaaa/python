a7c24e3858e20b74c40576266917e684c607d496
Malavika <146753226+malvikaaaaa@users.noreply.github.com>
2023-12-07 10:43:52 +0530

    Add files via upload

---
+++ graphics/__init__.py
@@ -0,0 +1,8 @@
+#import all packages
+
+from graphics  import circle as c
+from graphics.rectangle import *
+from graphics.dgraphics import cuboid
+from graphics.dgraphics import sphere
+
+#__all__=['Circle','Rectangle']
