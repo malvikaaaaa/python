8df4d1cae95405eeee8c2e1cfe7372b92e1a304d
Malavika <146753226+malvikaaaaa@users.noreply.github.com>
2024-02-01 00:17:05 +0530

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -3,24 +3,26 @@ class bank:
         self.name=n
         self.location=l
     def display(self):
-        print("Name:",self.name)
-        print("Location:",self.location)
+        print("Bank Name:",self.name)
+        print("Bank Location:",self.location)
 
 class insurance(bank):
     def __init__(self,n="nil",l="nil",amt=0):
         super().__init__(n,l)
         self.__amount=amt
-
-    def display(self):
+        
+    def display1(self):
         super().display()
+        
+    def display2(self):
         print("Insurance amount:",self.__amount)
 
     def __le__(self,other):
-        if(self.__amount<=other.__amount):
-            print("Amount 1 is lesser")
-        else:
-            print("Amount 2 is lesser")
-         #return(self.__amount<=other.__amount)
+        #if(self.__amount<=other.__amount):
+            #print("Amount 1 is lesser")
+        #else:
+            #print("Amount 2 is lesser")
+        return(self.__amount<=other.__amount)
 
 
 bname=input("Enter the bank name:")
@@ -30,7 +32,12 @@ iamt2=int(input("Enter insurance amt 2:"))
 
 in1=insurance(bname,bloc,iamt1)
 in2=insurance(bname,bloc,iamt2)
-in1.display()
-in2.display()
-in1<=in2
+in1.display1()
+in1.display2()
+in2.display2()
+if(in1<=in2):
+    print("Amount 1 is lesser")
+else:
+    print("Amount 2 is lesser")
+    
 #print("Two insurance amount are less than or equal",in1<=in2)
