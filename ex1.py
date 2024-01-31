8c7af7c79b7b2e2f1eca81c0befb196fdffb23ab
Malavika <146753226+malvikaaaaa@users.noreply.github.com>
2024-01-31 23:16:21 +0530

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,30 +1,36 @@
 class bank:
-    def __init__(self,n="Empty",l="Empty"):
+    def __init__(self,n="nil",l="nil"):
         self.name=n
         self.location=l
+    def display(self):
+        print("Name:",self.name)
+        print("Location:",self.location)
 
 class insurance(bank):
-    def __init__(self,n,l,amt=0):
+    def __init__(self,n="nil",l="nil",amt=0):
         super().__init__(n,l)
         self.__amount=amt
 
     def display(self):
-        print("Bank name: ",self.name)
-        print("Bank location: ",self.location)
+        super().display()
         print("Insurance amount:",self.__amount)
 
     def __le__(self,other):
-        return(self.__amount<=other.__amount)
+        if(self.__amount<=other.__amount):
+            print("Amount 1 is lesser")
+        else:
+            print("Amount 2 is lesser")
+         #return(self.__amount<=other.__amount)
 
 
 bname=input("Enter the bank name:")
 bloc=input("Enter the bank location:")
-iamt1=input("Enter insurance amt 1:")
-iamt2=input("Enter insurance amt 2:")
+iamt1=int(input("Enter insurance amt 1:"))
+iamt2=int(input("Enter insurance amt 2:"))
 
 in1=insurance(bname,bloc,iamt1)
 in2=insurance(bname,bloc,iamt2)
 in1.display()
 in2.display()
-print("Two insurance amount are less than or equal",in1<=in2)
-
+in1<=in2
+#print("Two insurance amount are less than or equal",in1<=in2)
